b3cc63fedcf714d25a5e58f5c714b0da0f731fb2
githubanhduong fake <20521225@gm.uit.edu.vn>
2024-07-20 15:56:10 +0700

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,23 @@ import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:namer_app/pages/weather_page.dart';
 
-
 const bool kReleaseMode = bool.fromEnvironment('dart.vm.product');
 
-void main() => runApp(MyApp());
+void main() => runApp(
+      DevicePreview(
+        enabled: !kReleaseMode,
+        builder: (context) => MyApp(), // Wrap your app
+      ),
+    );
 
 class MyApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      // ignore: deprecated_member_use
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
       home: WeatherPage(),
     );
